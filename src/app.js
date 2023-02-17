0edeb55588aa51e457b06f1832e41040b5c1aa0b
German Silva <german.silva.96@hotmail.com>
2023-02-16 19:29:08 -0600

    cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const db = require('./utils/database')
 const userRoutes = require('./routes/users.routes')
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 
 db.authenticate()
